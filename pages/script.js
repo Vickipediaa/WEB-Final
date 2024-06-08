c982bf842d4e9eb2ea8fcf28f337cc2400bc9a02
cplussssss <163500521+cplussssss@users.noreply.github.com>
2024-06-09 00:05:04 +0800

    Create script.js

---
+++ pages/script.js
@@ -0,0 +1,19 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("box_tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// 預設顯示登入頁
+document.getElementById("login").style.display = "block";
